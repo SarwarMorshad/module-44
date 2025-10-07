4c97d0d62973aa7215ef1f88c25f56104bd30db9
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-10-07 19:45:27 +0200

    React Form

---
+++ react-form/src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SimpleForm = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
