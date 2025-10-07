f6eda15ea15e43b72bb94f9c7c8a08194fa83501
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-10-07 20:32:58 +0200

    Recat form: Action

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import FormAction from "./components/FormAction/FormAction";
+import SimpleForm from "./components/SimpleForm/SimpleForm";
 
 function App() {
   return (
     <>
       <h1>Explore React Form</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      <FormAction></FormAction>
     </>
   );
 }
--- react-form/src/components/SimpleForm/SimpleForm.jsx
+++ react-form/src/components/SimpleForm/SimpleForm.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
 const SimpleForm = () => {
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" />
+        <input type="text" name="name" placeholder="Your Name" />
+        <br />
+        <input type="email" name="email" id="" placeholder="Your Email" />
         <br />
         <button type="submit">Submit</button>
       </form>
